770d8fd46a2348c1b7acfcd6bd29226de1211111
Pietro Paolo Carpentras <pietropaolo.carpentras@outlook.it>
2019-08-21 21:07:34 +0200

    Revert "Revert "Create button.php""
    This reverts commit 02dd512f07fcbcf7637d8746afa2c73f149714b3.

---
+++ resources/lang/it/button.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Buttons Language Lines
+  |--------------------------------------------------------------------------
+  */
+
+  'login' => 'Accedi',
+  'register' => 'Registrati',
+  'login_with_facebook' => 'Accedi con facebook',
+  'send' => 'Invia',
+  'send_message' => 'Invia messaggio',
+  'send_report' => 'Invia segnalazione',
+  'send_anwser' => 'Invia risposta',
+  'save_settings' => 'Salva impostazioni',
+  'save_changes' => 'Salva modifiche',
+  'password_changes' => 'Cambia password',
+  'delete_account' => 'Elimina account',
+  'forgot_password' => 'Hai dimenticato i dati di accesso?',
+  'no_account' => 'Non hai ancora un account? Creane uno',
+  'already_registered' => 'Hai già un account? Accedi',
+];
